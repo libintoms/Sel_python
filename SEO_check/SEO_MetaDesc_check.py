b77b9233e39ac32da9d8649c4a5448172d33a688
libintoms <libint20@gmail.com>
2020-06-03 09:53:08 +0530

    Meta description automation

--- SEO_check/SEO_MetaDesc_check.py
+++ SEO_check/SEO_MetaDesc_check.py
@@ -6,7 +6,7 @@ from openpyxl import load_workbook
 import sys
 sys.path.append("D:/OneDrive - CACTUS/Python/Sel_python")
 
-class Meta_keywords_check(unittest.TestCase):
+class Meta_descrip_check(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -16,10 +16,10 @@ class Meta_keywords_check(unittest.TestCase):
         cls.driver.implicitly_wait(10)
 
         #initaiting file reader
-        excel_file=r'D:/OneDrive - CACTUS/Python/Sel_python/SEO_check/SEO_metakeys_data.xlsx'
+        excel_file=r'D:/OneDrive - CACTUS/Python/Sel_python/SEO_check/SEO_metadesc_data.xlsx'
         cls.df=pd.read_excel(excel_file,sheet_name='Meta_desc')
         cls.Urls=cls.df['URLs']
-        cls.Meta_keys=cls.df['Meta keywords']
+        cls.Meta_desc=cls.df['Meta description']
 
         #initiating file writer
         cls.writer=pd.ExcelWriter(excel_file, engine='openpyxl')
@@ -27,7 +27,7 @@ class Meta_keywords_check(unittest.TestCase):
         cls.writer.book=cls.book
         cls.writer.sheets=dict((ws.title,ws)for ws in cls.book.worksheets)
 
-    def test_meta_key_verify(self):
+    def test_meta_desc_verify(self):
         i = 0
         j = (len(self.Urls))
         k = 0
@@ -37,12 +37,12 @@ class Meta_keywords_check(unittest.TestCase):
             page_url = self.Urls[i]
             self.driver.get(page_url)
             print(page_url)
-            keywords_data = self.Meta_keys[k]
-            keywords=self.driver.find_elements_by_name("keywords")
-            for elements in keywords:
-                site_keywords=elements.get_attribute('content')
-                if site_keywords==keywords_data:
-                    output = "Meta keywords are correct"
+            desc_data = self.Meta_desc[k]
+            decsrip=self.driver.find_elements_by_name("description")
+            for elements in decsrip:
+                site_descrip=elements.get_attribute('content')
+                if site_descrip==desc_data:
+                    output = "Meta description is correct"
                     print(output)
                     df1 = pd.DataFrame({'Status': [output]})
                     df1.to_excel(self.writer, sheet_name='Meta_desc', header=None, index=False, startrow=row_num,
@@ -50,7 +50,7 @@ class Meta_keywords_check(unittest.TestCase):
                     self.writer.save()
                     row_num = row_num + 1
                 else:
-                    output = "Keyword mismatch:"+site_keywords
+                    output = "Description mismatch:" + site_descrip
                     print(output)
                     df1 = pd.DataFrame({'Status': [output]})
                     df1.to_excel(self.writer, sheet_name='Meta_desc', header=None, index=False, startrow=row_num,
